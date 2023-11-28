2f0f41f9e8326147f35dab6bae97e0465cd311af
kebabnekaise <emsi06001@utb.vaxjo.se>
2023-11-28 10:55:12 +0100

    Add project files.

---
+++ inluppg5.6A/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace inluppg5_6A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in en multiplikation");
+
+            string input = Console.ReadLine();
+            string[] calculation = input.Split('*');
+
+            bool 
+            foreach (string s in calculation)
+            {
+                if (s.Length > 4)
+                {
+                    Console.WriteLine(s + " är för långt. Det får inte vara mer än 4 siffror");
+                    break;
+                }
+            }
+            int produkt = 1;
+            for (int i = 0; i < calculation.Length; i++)
+            {
+                produkt = produkt * int.Parse(calculation[i]);
+            }
+            Console.WriteLine("Produkten är " + produkt);
+
+        }
+    }
+}
